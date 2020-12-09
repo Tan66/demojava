91b9393de0eb5ca38b3c315876e64d566e6feaf1
ShadowRajnsih <rr66520@gmail.com>
2020-12-09 11:40:46 +0530

    first commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM ubuntu
+RUN apt-get update
+RUN apt-get install --fix-missing
+RUN apt-get install openjdk-8-jdk -y
+RUN apt-get install maven -y
+WORKDIR /usr/app
+COPY . .
+RUN mvn clean install
+WORKDIR /usr/app/target
+RUN ls -la
+RUN sh -c 'touch gcpcloudrunback-0.0.1-SNAPSHOT.jar'
+EXPOSE 8080
+CMD ["java","-jar","gcpcloudrunback-0.0.1-SNAPSHOT.jar"]
---
+++ src/main/java/dev/truaro/blog/gcpcloudrunback/HelloWorldController.java
@@ -0,0 +1,20 @@
+package dev.truaro.blog.gcpcloudrunback;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+}
